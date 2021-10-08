b06d05f68a67d04dcca8527669d70cbb362dceb6
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2021-10-08 14:19:01 +0100

    create a global reusable styles

---
+++ styles/global.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+
+  titleText: {
+    fontFamily: "Roboto-Bold",
+    fontSize: 18,
+  },
+
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
+
+export default globalStyles;
